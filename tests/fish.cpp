fff998fe0bce7712d6d7768389faa98e63e8c577
Nokome Bentley <nokome.bentley@trophia.com>
2016-08-31 10:06:23 +1200

    Added another fish test

--- tests/fish.cpp
+++ tests/fish.cpp
@@ -7,7 +7,7 @@
 BOOST_AUTO_TEST_SUITE(fish)
 
 /**
- * @brief      Test the construction of fish
+ * @brief      Test the construction of fish at birth
  */
 BOOST_AUTO_TEST_CASE(birth){
 	Fish fish;
@@ -15,11 +15,26 @@ BOOST_AUTO_TEST_CASE(birth){
 
 	BOOST_CHECK(fish.alive());
 
+	BOOST_CHECK_EQUAL(fish.region, HG);
+
 	BOOST_CHECK_EQUAL(fish.age(), 0);
 	BOOST_CHECK_EQUAL(fish.age_bin(), 0);
 
 	BOOST_CHECK_EQUAL(fish.length, 0);
 	BOOST_CHECK_EQUAL(fish.length_bin(), 0);
+}
+
+/**
+ * @brief      Test the construction of fish through seeding
+ */
+BOOST_AUTO_TEST_CASE(seed){
+	Fish fish;
+	fish.seed();
+
+	BOOST_CHECK(fish.alive());
+
+	BOOST_CHECK(fish.age() > 0);
+	BOOST_CHECK(fish.length > 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
